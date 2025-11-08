71fd226cff34e2fa702bcf4e621cb6a213174f21
Roger Allen <rallen@gmail.com>
2025-11-08 12:00:21 -0800

    fix forcefield

--- src/forcefield.c
+++ src/forcefield.c
@@ -81,23 +81,43 @@ bool UpdateForceField(ForceFieldManager* mgr, struct EnemyManager* emgr)
 
     return anyHit;
 }
-
+//
+// DrawForceField2D - Implementation Notes:
+// - 3 rotated squares horizontally in 2 rows for grid effect
+// - so, square diagonals align with screen axes and are 3 wide, 2 high
+// - square diagonal is min(screenWidth*0.8/3, screenHeight*0.8/2)
+// 
 void DrawForceField2D(ForceFieldManager* mgr)
 {
     if (mgr->state == FF_STATE_ACTIVE)
     {
-        int screenWidth = GetScreenWidth();
-        int screenHeight = GetScreenHeight();
-        int cellWidth = screenWidth / 4;
-        int cellHeight = screenHeight / 2;
-
-        for (int y = 0; y < 2; y++)
-        {
-            for (int x = 0; x < 4; x++)
-            {
-                DrawRectangleLines(x * cellWidth, y * cellHeight, cellWidth, cellHeight, COLOR_FORCEFIELD_GRID);
-            }
-        }
+        int ffWidth = GetScreenWidth() * 0.80f;
+        int ffHeight = GetScreenHeight() * 0.80f;
+        int cellDiagonal = (ffWidth / 3 < ffHeight / 2) ? (ffWidth / 3) : (ffHeight / 2);
+        int xc = GetScreenWidth()/2;
+        int yc = GetScreenHeight()/2;
+        // GRID is x0, x1, xc, x2, x3 and y0, y1, yc, y2, y3 (upper left origin)
+        int x0 = xc - cellDiagonal * 1.5f;
+        int x1 = xc - cellDiagonal;
+        int x2 = xc + cellDiagonal;
+        int x3 = xc + cellDiagonal * 1.5f;
+        int y0 = yc - cellDiagonal; 
+        int y1 = yc - cellDiagonal/2;
+        int y2 = yc + cellDiagonal/2;
+        int y3 = yc + cellDiagonal;
+        // Draw the lines that make up the grid, first to SE
+        DrawLine(x0, y2, x1, y3, COLOR_FORCEFIELD_GRID);
+        DrawLine(x0, y1, xc, y3, COLOR_FORCEFIELD_GRID);
+        DrawLine(x1, y0, x2, y3, COLOR_FORCEFIELD_GRID);
+        DrawLine(xc, y0, x3, y2, COLOR_FORCEFIELD_GRID);
+        DrawLine(x2, y0, x3, y1, COLOR_FORCEFIELD_GRID);
+        // then to NE
+        DrawLine(x0, y1, x1, y0, COLOR_FORCEFIELD_GRID);
+        DrawLine(x0, y2, xc, y0, COLOR_FORCEFIELD_GRID);
+        DrawLine(x1, y3, x2, y0, COLOR_FORCEFIELD_GRID);
+        DrawLine(xc, y3, x3, y1, COLOR_FORCEFIELD_GRID);
+        DrawLine(x2, y3, x3, y2, COLOR_FORCEFIELD_GRID);
+        
     }
 }
 
