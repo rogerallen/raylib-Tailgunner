0abf3734897d87a792c4680e5fd89e3fe429428d
Roger Allen <rallen@gmail.com>
2025-11-26 21:16:19 -0800

    match convention

--- resources/shaders/glsl330/starfield.fs
+++ resources/shaders/glsl330/starfield.fs
@@ -1,10 +1,10 @@
 #version 330 core
 
 // Output fragment color
-out vec4 finalColor;
+out vec4 fragColor;
 
 void main()
 {
     // VGREEN color: 0x30 0xf0 0xe0 0xff = (48, 240, 224, 255) normalized to (0.188, 0.941, 0.878, 1.0)
-    finalColor = vec4(0.188, 0.941, 0.878, 1.0);
+    fragColor = vec4(0.188, 0.941, 0.878, 1.0);
 }
